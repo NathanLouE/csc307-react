9b061343e8bd6156a9116c125394e458ee867031
Nathan Louie <nathanlou22@gmail.com>
2024-04-16 11:25:56 -0700

    add delete function

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -72,9 +72,18 @@ const addUser = (user) => {
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
-  console.log(req);
   addUser(userToAdd);
   res.send();
+});
+
+app.delete("/users/", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.delete(result);
+  }
 });
 
 app.listen(port, () => {
